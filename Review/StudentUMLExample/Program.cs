f320c507754014df1ca2c782d727385842bfc714
Elizabeth Sparks <Elizabethsparks@ou.edu>
2020-09-02 12:08:29 -0500

    almost finished student example

---
+++ Review/StudentUMLExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StudentUMLExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student myStudent = new Student();
+            myStudent.FirstName = "Adam";
+            myStudent.LastName = "Ackerman";
+            myStudent.IsOnProbation = true;
+            myStudent.SoonerID = 5;
+
+            Console.WriteLine(myStudent);
+        }
+    }
+}
---
+++ Review/StudentUMLExample/Student.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentUMLExample
+{
+    class Student
+    {
+        public int SoonerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsOnProbation { get; set; }
+        public double GPA { get; set; }
+
+        private double BursarBalance;
+
+        public Student()
+        {
+            SoonerID = -1;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            IsOnProbation = false;
+            GPA = 0;
+            BursarBalance = 1000;
+
+        }
+        
+        public Student(int id, string fName, string lName, double BursarBalance)
+        {
+            SoonerID = id;
+            FirstName = fName;
+            LastName = lName;
+            IsOnProbation = false;
+            GPA = 0;
+            this.BursarBalance = BursarBalance;
+        }
+
+        public void MakePayment(double amount)
+        {
+            //BursarBalance = BursarBalance - amount;
+            if (amount > 0)
+            {
+                BursarBalance -= amount;
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+
+        public double CheckBalance()
+        {
+            return BursarBalance;
+        }
+
+    }
+}
