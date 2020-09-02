7a4d85e85a0636bd765b76709c3d48e6fcdced73
Elizabeth Sparks <Elizabethsparks@ou.edu>
2020-09-02 11:34:52 -0500

    created file example

---
+++ Review/FilesExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace FilesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = ""; @"C:\Users\Elizabeth Sparks\Downloads\Pride and Prejudice by Jane Austen.txt
+           
+                var lines =  File.ReadAllText(filePath);
+          
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string currentLIne = lines[i];
+                if (currentLIne.Contains("Chapter"));
+                {
+                    Console.ReadKey();
+                }
+                Console.WriteLine(currentLIne);
+            }
+
+
+
+        }
+    }
+}
